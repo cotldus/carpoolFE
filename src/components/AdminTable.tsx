eaaf02fa3ca6cd037cf48b039431e4a210294c9b
Stephanie Chua <stephaniechua@Stephanies-MacBook-Air.local>
2023-03-02 01:58:43 +0800

    admin table form improved

--- src/components/AdminTable.tsx
+++ src/components/AdminTable.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,6 +7,16 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import axios from "axios";
+
+const config = {
+  headers: {
+    "content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -32,7 +42,7 @@ function createData(
   id: string,
   dateTime: string,
   departure: string,
-  pax: number,
+  pax: number
 ) {
   return { id, dateTime, departure, pax };
 }
@@ -46,60 +56,99 @@ const rows = [
 ];
 
 type Row = {
-    id: string;
-    dateTime: string;
-    departure: string;
-    pax: number;
-}
+  id: string;
+  dateTime: string;
+  departure: string;
+  pax: number;
+};
 
-const Row = ({row}: {row: Row}) => {
-    const [editMode, setEditMode] = useState(false);
-    if (editMode) return <EditLine setEditMode={setEditMode}/>
-    return (
-      <StyledTableRow>
-        <StyledTableCell component="th" scope="row">
-          {row.id}
-        </StyledTableCell>
-        <StyledTableCell align="right">{row.dateTime}</StyledTableCell>
-        <StyledTableCell align="right">{row.departure}</StyledTableCell>
-        <StyledTableCell align="right">{row.pax}</StyledTableCell>
-        <StyledTableCell align="right" onClick={()=> setEditMode(true)}>Edit/Duplicate</StyledTableCell>
-      </StyledTableRow>
-    );
-  }
+const Row = ({ row }: { row: Row }) => {
+  const [editMode, setEditMode] = useState(false);
+  if (editMode) return <EditLine setEditMode={setEditMode} id={row.id} />;
+  return (
+    <StyledTableRow>
+      <StyledTableCell component="th" scope="row">
+        {row.id}
+      </StyledTableCell>
+      <StyledTableCell align="right">{row.dateTime}</StyledTableCell>
+      <StyledTableCell align="right">{row.departure}</StyledTableCell>
+      <StyledTableCell align="right">{row.pax}</StyledTableCell>
+      <StyledTableCell align="right" onClick={() => setEditMode(true)}>
+        Edit/Duplicate
+      </StyledTableCell>
+    </StyledTableRow>
+  );
+};
 
-  const EditLine = ({setEditMode}: {setEditMode: Dispatch<SetStateAction<boolean>>}) => {
-    return (
-        <StyledTableRow>
-          <StyledTableCell component="th" scope="row">
-            <input/>
-          </StyledTableCell>
-          <StyledTableCell align="right"><input/></StyledTableCell>
-          <StyledTableCell align="right"><input/></StyledTableCell>
-          <StyledTableCell align="right"><input/></StyledTableCell>
-          <StyledTableCell align="right" onClick={()=> setEditMode(false)}>Save</StyledTableCell>
-        </StyledTableRow>
-      );
-    }
+const EditLine = ({
+  setEditMode,
+  id,
+}: {
+  setEditMode: Dispatch<SetStateAction<boolean>>;
+  id: string;
+}) => {
+    const value = {setEditMode, id}
+  return (
+    <StyledTableRow>
+      <StyledTableCell component="th" scope="row">
+      <TextField name="id" variant="outlined" size="small" value={id} read-only/>
+      </StyledTableCell>
+      <StyledTableCell align="right">
+        <TextField name="dateTime" variant="outlined" size="small"/>
+      </StyledTableCell>
+      <StyledTableCell align="right">
+        <TextField name="departure" variant="outlined" size="small" />
+      </StyledTableCell>
+      <StyledTableCell align="right">
+        <TextField name="pax" variant="outlined" size="small" />
+      </StyledTableCell>
+      <StyledTableCell align="right">
+        <Button type="submit" variant="outlined" value={id} onClick={(e)=> {
+            e.preventDefault();
+            setEditMode(false)
+        }}>
+          Save
+        </Button>
+      </StyledTableCell>
+    </StyledTableRow>
+  );
+};
+
+const handleSubmit = (e: any) => {
+  // Prevent the browser from reloading the page
+  e.preventDefault();
+
+  // Read the form data
+  const form = e.target;
+  const formData = new FormData(form);
+
+  const formJson = Object.fromEntries(formData.entries());
+  console.log(formJson)
+  axios.post("/save", {
+    ...formJson
+  }, config).catch((e)=> console.log("error"));
+};
 
 const AdminTable = () => {
   return (
     <TableContainer component={Paper}>
-        <form>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Journey ID</StyledTableCell>
-            <StyledTableCell align="right">Date/ Time</StyledTableCell>
-            <StyledTableCell align="right">Depature</StyledTableCell>
-            <StyledTableCell align="right">Pax</StyledTableCell>
-            <StyledTableCell align="right">Actions</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => <Row row={row} key={row.id}/>)}
-        </TableBody>
-      </Table>
+      <form onSubmit={handleSubmit} method="post" action="#">
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Journey ID</StyledTableCell>
+              <StyledTableCell align="right">Date/ Time</StyledTableCell>
+              <StyledTableCell align="right">Depature</StyledTableCell>
+              <StyledTableCell align="right">Pax</StyledTableCell>
+              <StyledTableCell align="right">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <Row row={row} key={row.id} />
+            ))}
+          </TableBody>
+        </Table>
       </form>
     </TableContainer>
   );
